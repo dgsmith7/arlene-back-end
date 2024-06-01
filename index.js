68c005db3d5d0048a783217996a44d350fcd2bed
dgsmith7@hotmail.com <dgsmith7@hotmail.com>
2024-05-31 22:31:57 -0500

    failed first atempt with test key.  will resolve with local dev then redploy

--- index.js
+++ index.js
@@ -179,12 +179,12 @@ app.post("/advise", async function (req, res) {
     //const users = await db.getCollection("bbcUsers").find({});
     //console.log(db);
 
-    const completion = await openai.chat.completions.create({
-      messages: [{ role: "system", content: "You are a helpful assistant." }],
-      model: "gpt-3.5-turbo",
-    });
+    // const completion = await openai.chat.completions.create({
+    //   messages: [{ role: "system", content: "You are a helpful assistant." }],
+    //   model: "gpt-3.5-turbo",
+    // });
 
-    console.log(completion.choices[0]);
+    // console.log(completion.choices[0]);
 
     return (
       res
