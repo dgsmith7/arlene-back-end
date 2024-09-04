fb9536b1d41f6a2af486bcd994b5ebac00d55351
dgsmith7@hotmail.com <dgsmith7@hotmail.com>
2024-09-04 09:18:29 -0500

    changed seesion option

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ const sessOptions = {
   //  httpOnly: true, // set as default - maybe need the remove is not viewable
   secret: process.env.SECRET_KEY,
   resave: false,
-  saveUninitialized: true,
+  saveUninitialized: false,
   store: sessStore, //new MongoStore({ mongoUrl: db.client.s.url }),
   //  maxAge: 7200000, //2 hours
   cookie: {
