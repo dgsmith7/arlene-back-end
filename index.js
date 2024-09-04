43c81eeef95832766d61f3dd36635ce3389285d1
dgsmith7@hotmail.com <dgsmith7@hotmail.com>
2024-09-04 12:20:01 -0500

    changed login sequence

--- index.js
+++ index.js
@@ -120,7 +120,7 @@ const corsOptionsProd = {
   credentials: true,
   allowedHeaders: ["Accept", "Content-Type"],
   origin: [
-    "https://aviation-readiness-app-sdbks.ondigitalocean.app",
+    "https://aviation-readiness-app-8eg86.ondigitalocean.app",
     "https://arlene-app.com",
   ],
   methods: ["POST", "GET", "OPTIONS"],
