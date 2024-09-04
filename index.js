bc1aa3a0f68f85a5b14140734a486e87b324ac27
dgsmith7@hotmail.com <dgsmith7@hotmail.com>
2024-09-04 11:56:02 -0500

    changed login sequence

--- index.js
+++ index.js
@@ -256,21 +256,22 @@ app.post(
   ],
   passport.authenticate("local", {
     failureRedirect: "/login-failure",
-    //successRedirect: "/login-success",
+    // successRedirect: "/login-success",
   }),
   (req, res, err, next) => {
-    console.log("passport user: ", req.user);
-    console.log("login-success:", req.sessionID);
-    console.log("passport user: ", req.user);
-    logger.log({
-      level: "info",
-      //    message: `Successful 1FA login for ${req.session.passport.user} with ${req.session.passport.privileges}.`,
-      message: `Successful 1FA login for  with .`,
-    });
-    res.status(200).send({
-      message: "Login successful",
-      privileges: req.user.privileges,
-    });
+    res.redirect("/login-success");
+    // console.log("passport user: ", req.user);
+    // console.log("login-success:", req.sessionID);
+    // console.log("passport user: ", req.user);
+    // logger.log({
+    //   level: "info",
+    //   //    message: `Successful 1FA login for ${req.session.passport.user} with ${req.session.passport.privileges}.`,
+    //   message: `Successful 1FA login for  with .`,
+    // });
+    // res.status(200).send({
+    //   message: "Login successful",
+    //   privileges: req.user.privileges,
+    // });
 
     // res.status(200).send({
     //   message: "Login successful",
